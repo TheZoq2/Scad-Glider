1548f6f4dcbf7a861c0412e15a54d3bf10b6bd5f
Frans Skarman <frans.skarman@gmail.com>
2016-06-06 17:44:45 +0200

    Add glider shape function

--- glider.scad
+++ glider.scad
@@ -5,10 +5,34 @@ module cut_cone(radius1, radius2, height)
     hull()
     {
         cylinder(r=radius1, h=0.1);
-        translate([0,0.3,1] * height)
-        scale([1, 1.2, 1])
+        translate([0,0,1] * height)
         cylinder(r=radius2, h=0.1);
     };
 }
 
-cut_cone(20, 25, 25);
+//Array of sections that will make up the body
+//Contains  the radius  of the section followed by the length
+section_amount = 5;
+sections = [
+    [25, 60],
+    [35, 80],
+    [40, 80],
+    [35, 100],
+    [10, 110],
+];
+
+
+current_offset = 0;
+for(i = [0: section_amount - 2])
+{
+    translate([0,0,$current_offset])
+    cut_cone(sections[i][0], sections[i + 1][0], sections[i][1]);
+
+    //let(current_offset=current_offset + sections[i][1]);
+    let(current_offset,0);
+
+    echo("New  section");
+    echo(current_offset);
+    echo(sections[i][1]);
+}
+cylinder(r=1, h=350);
--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,123 @@ extern crate scad_generator;
 //Avoid having to write scad_generator:: everywhere
 use scad_generator::*;
 
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
+
+fn write_result(object: &ScadObject)
+{
+    //Writing the result to file
+    let path = Path::new("glider_auto.scad");
+
+    // Open a file in write-only mode, returns `io::Result<File>`
+    let mut file = match File::create(&path) {
+        Err(_) => panic!("couldn't write file"),
+        Ok(file) => file,
+    };
+
+    match file.write(object.get_code().as_bytes()) {
+        Err(_) => panic!("Failed to write output file"),
+        Ok(_) => {}
+    };
+}
+
+fn cut_cone(d1: f32, d2: f32, height: f32) -> ScadObject
+{
+    let cyl_height = 0.0000001;
+    scad!(Hull; 
+        {
+            scad!(Cylinder(cyl_height, Diameter(d1))),
+
+            scad!(Translate(vec3(0.0, 0.0, height));{
+                scad!(Cylinder(cyl_height, Diameter(d2)))
+            }),
+        })
+}
+
+fn servo() -> ScadObject
+{
+    let box_size = vec3(24.0, 13.0, 22.0);
+    let screw_diameter = 2.0;
+    let screw_mount_size = vec3(33.0, box_size.y, 2.5);
+
+    let mount_offset = vec3(0.0, 0.0, 16.0);
+
+    let mut base_cube = scad!(
+        Translate(-vec3(box_size.x / 2.0, 0.0, 0.0));
+        {
+            scad!(Cube(box_size))
+        });
+
+    let screw_mount = scad!(
+        Translate(
+                -vec3(screw_mount_size.x / 2.0, 0.0, 0.0) +
+                mount_offset
+            );
+        {
+            scad!(Cube(screw_mount_size))
+        });
+
+    scad!(Union;{base_cube, screw_mount})
+}
+
+fn glider_body_shape() -> ScadObject
+{
+    let segments = [
+            (25.0, 34.0, 20.0),
+            (34.0, 40.0, 40.0),
+            (40.0, 45.0, 80.0),
+            (45.0, 40.0, 80.0),
+            (40.0, 10.0, 140.0),
+        ];
+
+    let mut result = scad!(Union);
+
+    let mut current_height = 0.0;
+    for i in 0..segments.len()
+    {
+        result.add_child(scad!(Translate(vec3(0.0, 0.0, current_height));
+            {
+                cut_cone(segments[i].0, segments[i].1, segments[i].2),
+            }));
+
+        current_height += segments[i].2;
+
+    }
+
+    result
+}
+fn glider_body() -> ScadObject
+{
+    let inner_scale = 0.95;
+
+    scad!(Difference;
+    {
+        glider_body_shape(),
+        scad!(Scale(vec3(inner_scale, inner_scale, inner_scale));
+        {
+            glider_body_shape(),
+        }),
+    })
+}
+
+
+
 pub fn main()
 {
     //Create an scad object
-    let translation = scad!(Translate(vec3(2.0, 2.0, 3.0));
+    let translation = 
+        scad!(Difference;
+        {
+            glider_body(),
+            scad!(Translate(vec3(-40.0, -40.0, 30.0));
             {
-                scad!(Cube(vec3(2.0,1.0,4.0)))
-            }
-        );
+                scad!(Cube(vec3(80.0, 80.0, 400.0))),
+            })
+        });
 
+
+    write_result(&translation);
 
     //Print the result
     println!("{}", translation.get_code());
